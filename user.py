7e4c851a8400906b630c4b597ab8dbc306afa61d
HenryDu <tiantianplayworld@gmail.com>
2018-06-21 15:42:18 +0800

    done

--- user.py
+++ user.py
@@ -148,5 +148,7 @@ if __name__ == '__main__':
 
     # 如果低于1200 分配最低数量食材
     elif metabolism_data <= 1200:
+        print("由于您的基础代谢低于1200大卡,所以我们无法为您提健康的简直餐饮计划")
+        print("以下列出为1200大卡饮食计划,希望您能勤加锻炼,增加基础代谢")
         low_calorie = Calorie()
         low_calorie.food_list_low()
